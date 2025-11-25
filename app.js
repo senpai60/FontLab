9f9bd4014def59c9ee72793a1e245272aa27b2b7
Shivam Rajput <shiv.coder9@gmail.com>
2025-11-26 04:37:56 +0530

    feat: implement font card generation with data import

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 import { asideToggle } from "./src/components/asideToggle.js";
+import { fontData } from "./data/fontData.js";
 
 asideToggle();
+
+let cardWrapper = document.querySelector('.wrapper');
+
+fontData.forEach((elem) => {
+  let fontCardDiv = document.createElement('div');
+  fontCardDiv.classList.add('font-card');
+
+  fontCardDiv.innerHTML = `<h1>Heading</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
+              ducimus voluptatibus omnis ipsum quaerat quibusdam nam vitae,
+              dolorem possimus totam debitis magni mollitia quos atque deserunt
+              assumenda facere, qui quas.
+            </p>`;
+
+  cardWrapper.appendChild(fontCardDiv);
+});
---
+++ data/fontData.js
@@ -0,0 +1,242 @@
+export const fontData = [
+  {
+    "id": 1,
+    "category": "Luxury & Elegant",
+    "pair_name": "Playfair Display + Raleway",
+    "header_font": "Playfair Display",
+    "body_font": "Raleway",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=Raleway:wght@300;400;500&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Playfair Display', serif;",
+      "body": "font-family: 'Raleway', sans-serif;"
+    }
+  },
+  {
+    "id": 2,
+    "category": "Luxury & Elegant",
+    "pair_name": "Cinzel + Fauna One",
+    "header_font": "Cinzel",
+    "body_font": "Fauna One",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Fauna+One&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Cinzel', serif;",
+      "body": "font-family: 'Fauna One', serif;"
+    }
+  },
+  {
+    "id": 3,
+    "category": "Luxury & Elegant",
+    "pair_name": "Prata + Lato",
+    "header_font": "Prata",
+    "body_font": "Lato",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Prata&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Prata', serif;",
+      "body": "font-family: 'Lato', sans-serif;"
+    }
+  },
+  {
+    "id": 4,
+    "category": "Luxury & Elegant",
+    "pair_name": "Cormorant Garamond + Proza Libre",
+    "header_font": "Cormorant Garamond",
+    "body_font": "Proza Libre",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Proza+Libre:wght@400;500&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Cormorant Garamond', serif;",
+      "body": "font-family: 'Proza Libre', sans-serif;"
+    }
+  },
+  {
+    "id": 5,
+    "category": "Modern & Minimalist",
+    "pair_name": "Montserrat + Open Sans",
+    "header_font": "Montserrat",
+    "body_font": "Open Sans",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700;800&family=Open+Sans:wght@300;400;600&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Montserrat', sans-serif;",
+      "body": "font-family: 'Open Sans', sans-serif;"
+    }
+  },
+  {
+    "id": 6,
+    "category": "Modern & Minimalist",
+    "pair_name": "Oswald + Inter",
+    "header_font": "Oswald",
+    "body_font": "Inter",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&family=Oswald:wght@500;700&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Oswald', sans-serif;",
+      "body": "font-family: 'Inter', sans-serif;"
+    }
+  },
+  {
+    "id": 7,
+    "category": "Modern & Minimalist",
+    "pair_name": "Manrope + Roboto",
+    "header_font": "Manrope",
+    "body_font": "Roboto",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@600;700;800&family=Roboto:wght@300;400;500&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Manrope', sans-serif;",
+      "body": "font-family: 'Roboto', sans-serif;"
+    }
+  },
+  {
+    "id": 8,
+    "category": "Modern & Minimalist",
+    "pair_name": "Barlow + Barlow Condensed",
+    "header_font": "Barlow Condensed",
+    "body_font": "Barlow",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@600;700;800&family=Barlow:wght@300;400;500&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Barlow Condensed', sans-serif;",
+      "body": "font-family: 'Barlow', sans-serif;"
+    }
+  },
+  {
+    "id": 9,
+    "category": "Tech & Startup",
+    "pair_name": "Space Grotesk + Inter",
+    "header_font": "Space Grotesk",
+    "body_font": "Inter",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&family=Space+Grotesk:wght@500;700&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Space Grotesk', sans-serif;",
+      "body": "font-family: 'Inter', sans-serif;"
+    }
+  },
+  {
+    "id": 10,
+    "category": "Tech & Startup",
+    "pair_name": "Rubik + Karla",
+    "header_font": "Rubik",
+    "body_font": "Karla",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Karla:wght@300;400;500&family=Rubik:wght@500;700;900&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Rubik', sans-serif;",
+      "body": "font-family: 'Karla', sans-serif;"
+    }
+  },
+  {
+    "id": 11,
+    "category": "Tech & Startup",
+    "pair_name": "IBM Plex Mono + IBM Plex Sans",
+    "header_font": "IBM Plex Mono",
+    "body_font": "IBM Plex Sans",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@500;700&family=IBM+Plex+Sans:wght@300;400;600&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'IBM Plex Mono', monospace;",
+      "body": "font-family: 'IBM Plex Sans', sans-serif;"
+    }
+  },
+  {
+    "id": 12,
+    "category": "Tech & Startup",
+    "pair_name": "Work Sans + Roboto",
+    "header_font": "Work Sans",
+    "body_font": "Roboto",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&family=Work+Sans:wght@600;800&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Work Sans', sans-serif;",
+      "body": "font-family: 'Roboto', sans-serif;"
+    }
+  },
+  {
+    "id": 13,
+    "category": "Editorial & Publishing",
+    "pair_name": "Merriweather + Lato",
+    "header_font": "Merriweather",
+    "body_font": "Lato",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Merriweather:ital,wght@0,400;0,700;1,400&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Merriweather', serif;",
+      "body": "font-family: 'Lato', sans-serif;"
+    }
+  },
+  {
+    "id": 14,
+    "category": "Editorial & Publishing",
+    "pair_name": "Libre Baskerville + Source Sans 3",
+    "header_font": "Libre Baskerville",
+    "body_font": "Source Sans 3",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Source+Sans+3:wght@300;400;600&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Libre Baskerville', serif;",
+      "body": "font-family: 'Source Sans 3', sans-serif;"
+    }
+  },
+  {
+    "id": 15,
+    "category": "Editorial & Publishing",
+    "pair_name": "Lora + Montserrat",
+    "header_font": "Lora",
+    "body_font": "Montserrat",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,600;1,400&family=Montserrat:wght@400;500;700&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Lora', serif;",
+      "body": "font-family: 'Montserrat', sans-serif;"
+    }
+  },
+  {
+    "id": 16,
+    "category": "Editorial & Publishing",
+    "pair_name": "Bitter + Open Sans",
+    "header_font": "Bitter",
+    "body_font": "Open Sans",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,700;1,500&family=Open+Sans:wght@300;400;600&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Bitter', serif;",
+      "body": "font-family: 'Open Sans', sans-serif;"
+    }
+  },
+  {
+    "id": 17,
+    "category": "Creative & Display",
+    "pair_name": "Syne + Plus Jakarta Sans",
+    "header_font": "Syne",
+    "body_font": "Plus Jakarta Sans",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700&family=Syne:wght@600;700;800&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Syne', sans-serif;",
+      "body": "font-family: 'Plus Jakarta Sans', sans-serif;"
+    }
+  },
+  {
+    "id": 18,
+    "category": "Creative & Display",
+    "pair_name": "Abril Fatface + Poppins",
+    "header_font": "Abril Fatface",
+    "body_font": "Poppins",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins:wght@300;400;500&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Abril Fatface', cursive;",
+      "body": "font-family: 'Poppins', sans-serif;"
+    }
+  },
+  {
+    "id": 19,
+    "category": "Creative & Display",
+    "pair_name": "Bebas Neue + Montserrat",
+    "header_font": "Bebas Neue",
+    "body_font": "Montserrat",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400;500&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'Bebas Neue', cursive;",
+      "body": "font-family: 'Montserrat', sans-serif;"
+    }
+  },
+  {
+    "id": 20,
+    "category": "Creative & Display",
+    "pair_name": "DM Serif Display + DM Sans",
+    "header_font": "DM Serif Display",
+    "body_font": "DM Sans",
+    "import_code": "@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=DM+Serif+Display:ital@0;1&display=swap');",
+    "css_usage": {
+      "header": "font-family: 'DM Serif Display', serif;",
+      "body": "font-family: 'DM Sans', sans-serif;"
+    }
+  }
+];
