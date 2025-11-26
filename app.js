2fda6441c0423693a03d4bb9ffc4e0ff9e8e811b
Shivam Rajput <shiv.coder9@gmail.com>
2025-11-26 17:05:29 +0530

    feat: implement save functionality for font data with local storage

--- app.js
+++ app.js
@@ -10,3 +10,33 @@ burgerBtn.addEventListener("click", asideToggle);
 fontCard(fontData);
 
 cardEvents(fontData);
+
+
+/* -------------------------- */
+
+// addPlayListBtn => click karga to form kulega;
+// add to localStrage on the click;
+
+const saveFont = (fontData) => {
+  const saveFontBtn = document.querySelectorAll('.save-font');
+
+  let savedList = [];
+  
+  saveFontBtn.forEach((elem, idx) => {
+    elem.addEventListener('click', () => {
+      const currentFontData = {
+        fontData: fontData[idx],
+        isSaved: true
+      };
+      savedList.push(currentFontData);
+      localStorage.setItem('saved', savedList);
+      const fontSavedItems = localStorage.getItem('saved');
+      console.log(fontSavedItems);
+    });
+  });
+  
+
+}
+
+
+saveFont(fontData);
